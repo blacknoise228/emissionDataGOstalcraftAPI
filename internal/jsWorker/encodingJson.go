5773835969e50e905b12622361959bc5d88884dd
blacknoise <blacknoise228@gmail.com>
2024-09-29 15:34:14 +0500

    hotfix

--- internal/jsWorker/encodingJson.go
+++ internal/jsWorker/encodingJson.go
@@ -8,7 +8,6 @@ import (
 
 // Decoding json to structure
 func EncodingJson(resp *http.Response) (EmissionInfo, error) {
-	defer resp.Body.Close()
 	var EmissionData EmissionInfo
 	if err := json.NewDecoder(resp.Body).Decode(&EmissionData); err != nil {
 		fmt.Println(err)
